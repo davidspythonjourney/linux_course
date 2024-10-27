acad2e7c51587321e6a810f74c08a409466987cf
davidspythonjourney <141944927+davidspythonjourney@users.noreply.github.com>
2024-10-27 16:57:58 +0200

    changed shell command for time top python time module.

--- server_script.py
+++ server_script.py
@@ -2,6 +2,7 @@ import os
 from collections import Counter
 import subprocess
 import json
+import time
 
 def readFile(file_name: str):
     if os.path.exists(file_name):
@@ -15,11 +16,15 @@ def grepLogToCounter():
         try:
             join_search_words = "|".join(key_words)
             command = f'grep -E "{join_search_words}" {grep_file}'
-            timestamp = int(subprocess.run(['date', '+%s'], capture_output=True, text=True).stdout.strip())
             result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
             count_word = Counter(result.stdout.split())
-            csv_info = {"time": timestamp, "info": count_word.get("INFO"), "error": count_word.get("ERROR"), "warn": count_word.get("WARN") }
-            print(json.dumps(csv_info))
+            csv_info = {
+                "time": int(time.time()),
+                "info": count_word.get(key_words[0]), 
+                "error": count_word.get(key_words[1]), 
+                "warn": count_word.get(key_words[2]) 
+            }
+                print(json.dumps(csv_info))
         except subprocess.CalledProcessError as e:
             print(f"Command '{command}' returned non-zero exit status {e.returncode}")
             print(f"Error output: {e.stderr}")
